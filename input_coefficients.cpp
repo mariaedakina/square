6c9c3b2a407426bdf808f587f4259dc3e106cc14
mariaedakina <117815104+mariaedakina@users.noreply.github.com>
2024-08-26 16:53:03 +0300

    Update input_coefficients.cpp

--- input_coefficients.cpp
+++ input_coefficients.cpp
@@ -4,33 +4,30 @@
 #include <assert.h>
 #include "input_coefficients.h"
 
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <assert.h>
-#include "input_coefficients.h"
-
 /**
  * @file input_coefficients.cpp
  * @brief Файл, содержащий функции для ввода коэффициентов квадратного уравнения.
  */
 
 int InputCoefficients(struct Koefs* k) {
-    assert(k != NULL); // Проверка на NULL
-    printf("# enter a, b, c\n");
-    char ch;
+    assert(k != NULL);
 
-    // Ввод коэффициентов
-    if (scanf("%lg %lg %lg", &k->koef_a, &k->koef_b, &k->koef_c) != 3) {
-        printf("error? try again\n");
-        return 1; // Ошибка ввода
+    while (true) {
+        printf("# enter numbers for a, b, c\n");
+
+        if (scanf("%lg %lg %lg", &k->koef_a, &k->koef_b, &k->koef_c) == 3) {
+            if (getchar() != '\n') {
+                printf("error? try again\n");
+
+                while (getchar() != '\n' && getchar() != EOF);
+                continue;
+            }
+            break;
+        } else {
+            printf("error? try again\n");
+            while (getchar() != '\n');
+        }
     }
 
-    // Проверка на наличие лишних символов после ввода
-    if (scanf("%c", &ch) == 1 && ch != '\n') {
-        printf("error? try again\n");
-        return 1; // Ошибка ввода
-    }
-
-    return 0; // Успешный ввод
+    return 0;
 }
